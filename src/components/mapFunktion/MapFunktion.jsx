215c01e7632090d84d69ef1c481dd815edffc5c5
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-23 15:44:25 +0100

    testing

--- src/components/mapFunktion/MapFunktion.jsx
+++ src/components/mapFunktion/MapFunktion.jsx
@@ -42,77 +42,96 @@ const html = movies.map((obj, index) => {
 const MapFunktion = () => {
 
     // useState movies
-const [sollMovies, setSollMovies] = useState(movies)
-console.log(movies)
-console.log(sollMovies)
+    const [sollMovies, setSollMovies] = useState(movies)
+    console.log(movies)
+    console.log(sollMovies)
 
-const sollMoviesCopy = [...sollMovies]
-// auslöser fürs sortieren 
-// kopie erstellen, damit original gleich bleibt
-function handleSortByRate(){
-    //const sollMoviesCopy = [...sollMovies]
-   //  console.log(sollMoviesCopy)
+    const sollMoviesCopy = [...sollMovies]
+    // auslöser fürs sortieren 
+    // kopie erstellen, damit original gleich bleibt
+    function handleSortByRate() {
+        //const sollMoviesCopy = [...sollMovies]
+        //  console.log(sollMoviesCopy)
 
-// copy sortieren nach Rate
-// sollMovies    anstatt sollMoviesCopy
-sollMoviesCopy.sort((a, b) => b.rate - a.rate)  // 9-0 absteigend sortieren
-console.log(sollMovies)
-console.log(sollMoviesCopy)
+        // copy sortieren nach Rate
+        // sollMovies    anstatt sollMoviesCopy
+        sollMoviesCopy.sort((a, b) => b.rate - a.rate)  // 9-0 absteigend sortieren
 
-// useState updaten 
-// ! return hin, dann wird auch der Desktop aktualisiert
-setSollMovies(sollMoviesCopy)
+        // useState updaten 
+        // ! return hin, dann wird auch der Desktop aktualisiert
+        setSollMovies(sollMoviesCopy)
 
-// ! Ende function ..Sort..Rate
-}
+    }
 
-// function  Jahr aufsteigend sortieren 
-function handleSortDateAsc(){
-    sollMoviesCopy.sort((a, b) => a.year - b.year)
+    // function  Jahr aufsteigend sortieren 
+    function handleSortDateAsc() {
+        sollMoviesCopy.sort((a, b) => a.year - b.year)
+        setSollMovies(sollMoviesCopy)
+
+    }
+
+    // function  Jahr absteigend sortieren 
+    function handleSortDateDes() {
+        sollMoviesCopy.sort((a, b) => b.year - a.year)
+        setSollMovies(sollMoviesCopy)
+    }
+
+    // function A-Z aufsteigend sortieren 
+    function handleSortAZ() {
+        sollMoviesCopy.sort((a, b) => (a.title < b.title ? -1 : 1))
+        // ? -1 : 1 
+        // if (a.title < b.title) true dann sortiere   -1   eins weiter for     else     1    eins weiter hinter
+        setSollMovies(sollMoviesCopy)
+    }
+
+    // function Z-A absteigend sortieren 
+    function handleSortZA() {
+        sollMoviesCopy.sort((a, b) => {
+            if (a.title > b.title) {
+                return -1;
+            } else {
+                (a.title > b.title) 
+                    return 1;
+                
+            }
+        }
+        
+        )
+        setSollMovies(sollMoviesCopy)
+    }
+
+
     console.log(sollMovies)
     console.log(sollMoviesCopy)
-    setSollMovies(sollMoviesCopy)
 
-    // ! Ende function..Sort..DateAsc
-}
+    // map Funktion 
+    const html = sollMovies.map((obj, index) => {
+        console.log(obj)
+        console.log(index)
+        console.log(uuidv4())
+        return <Schablone
+            key={uuidv4()}
+            key_={uuidv4()}
+            key_i={`obj_` + index + `_` + uuidv4()}
+            title={obj.title}
+            year={obj.year}
+            director={obj.director}
+            duration={obj.duration}
+            rate={obj.rate}
+            genre={obj.genre}  ></Schablone>
 
-// function  Jahr absteigend sortieren 
-function handleSortDateDes(){
-    sollMoviesCopy.sort((a, b) => b.year - a.year)
-    setSollMovies(sollMoviesCopy)
-}
+    })
 
-
-console.log(sollMovies)
-console.log(sollMoviesCopy)
-
-// map Funktion 
-const html = sollMovies.map((obj, index) => {
-    console.log(obj)
-    console.log(index)
-    console.log(uuidv4())
-    return <Schablone
-        key={uuidv4()}
-        key_={uuidv4()}
-        key_i={`obj_` + index + `_` + uuidv4()}
-        title={obj.title}
-        year={obj.year}
-        director={obj.director}
-        duration={obj.duration}
-        rate={obj.rate}
-        genre={obj.genre}  ></Schablone>
-
-})
 
 
     return (
 
         <>
-        <button onClick={handleSortDateAsc}>Sort by Date Ascending</button>
-        <button onClick={handleSortDateDes}>Sort by Date Descending</button>
-        <button onClick={handleSortByRate}>Best Rate</button>
-        <button>A-Z</button>
-        <button>Z-A</button>
+            <button onClick={handleSortDateAsc}>Sort by Date Ascending</button>
+            <button onClick={handleSortDateDes}>Sort by Date Descending</button>
+            <button onClick={handleSortByRate}>Best Rate</button>
+            <button onClick={handleSortAZ}>A-Z</button>
+            <button onClick={handleSortZA}>Z-A</button>
             {html}
         </>
 
